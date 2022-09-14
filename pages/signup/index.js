14139686630984e436c6804d26e231b3f92e702b
Uma2708 <bhardwajuma001@gmail.com>
2022-09-14 16:56:37 +0530

    :lipstick: signup UI completed

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import TextField from "@mui/material/TextField";
 import Image from "next/image"
 import logo from '../../assets/Instagram.jpeg'
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import IconButton from "@mui/material/IconButton";
+
 
 function index() {
     return (
@@ -33,6 +37,35 @@ function index() {
                     fullWidth
                     margin="dense"
                 />
+                <Button
+                    color="secondary"
+                    variant="outlined"
+                    component="label"
+                    fullWidth
+                    size="small"
+                >
+                    <IconButton color="secondary">
+                        <CloudUploadIcon />
+                    </IconButton>
+                    Upload Profile Image
+                    <input hidden accept="image/*" multiple type="file" />
+                </Button>
+
+                <Button
+                    style={{ marginTop: "1rem" }}
+                    variant="contained"
+                    component="label"
+                    fullWidth
+                >
+                    Sign Up
+                </Button>
+                <div className="tnc" >
+                    By signing up, you agree to our Terms, Conditions and Cookies policy.
+                </div>
+            </div>
+            <div className="bottom-card">
+                Already Have an account ?{" "}
+                <span style={{ color: "blueviolet" }}>Login</span>
             </div>
         </div>
     );
