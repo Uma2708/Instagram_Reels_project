a5b1f3184f2d3bf449bb0d317222e38c0b1ed5e1
Uma2708 <bhardwajuma001@gmail.com>
2022-09-15 16:08:07 +0530

    :construction: navbar started

---
+++ components/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+function Feed() {
+    return (
+        <div className="feed-container">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Feed
